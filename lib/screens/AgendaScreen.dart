1e2b399f8193c70ef0a3f49319b73a3dd3800953
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-08-04 17:38:39 +0800

    Initial commit

---
+++ lib/screens/AgendaScreen.dart
@@ -0,0 +1,16 @@
+// Copyright 2019 Joshua de Guzman (https://joshuadeguzman.github.io). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class AgendaScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Sample Screen'),
+      ),
+    );
+  }
+}
